aad0e059c53409d178519b61586a4db82ce36846
SimonBRiley <43470764+SimonR1ley@users.noreply.github.com>
2023-08-02 22:02:50 +0200

    Initial Commit

---
+++ Sync/SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncApp.swift
+//  Sync
+//
+//  Created by Simon Riley on 2023/08/02.
+//
+
+import SwiftUI
+
+@main
+struct SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
